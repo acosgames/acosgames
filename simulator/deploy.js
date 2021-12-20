188d12aa5485394dbc8ee541879f3883ec873092
Joel <joelruiz2@gmail.com>
2021-12-20 02:52:06 -0600

    finished iframe scaling on simulator, updated deployment upload to increment version every time, working iframe scaling on platform

--- simulator/deploy.js
+++ simulator/deploy.js
@@ -1,14 +1,20 @@
 
 const axios = require('axios');
 const path = require('path');
-const yargs = require('yargs/yargs')
+// const yargs = require('yargs/yargs')
+// const { hideBin } = require('yargs/helpers')
 const fs = require('fs');
 var FormData = require('form-data');
-const argv = yargs(process.argv).argv
+// const argv = yargs(hideBin(process.argv)).argv
 
-let type = process.argv[process.argv.length - 2];
+// let type = process.argv[process.argv.length - 2];
 let apikey = process.argv[process.argv.length - 1];
 
+// let isScaled = argv.scaled;
+// let apikey = argv.key;
+
+console.log(process.argv);
+// console.log(argv);
 
 async function deployAll() {
     let url = 'http://localhost:8080/api/v1/dev/update/game/bundle/';
@@ -19,6 +25,7 @@ async function deployAll() {
 
     var form_data = new FormData();
     form_data.append('apikey', apikey || '');
+    // form_data.append('scaled', isScaled);
     form_data.append("client", clientFile);
 
     try {
@@ -55,121 +62,6 @@ async function deployAll() {
     catch (e) {
         console.error(e);
     }
-
-}
-
-async function deployClient() {
-
-    let url = 'http://localhost:8080/api/v1/dev/update/client/bundle/';
-
-    console.log("Current Working Directory: ", process.cwd())
-    let filepath = path.resolve(process.cwd() + '/builds/client/client.bundle.js');
-    var newFile = fs.createReadStream(filepath);
-
-    var form_data = new FormData();
-    form_data.append('apikey', apikey || '');
-    form_data.append("bundle", newFile);
-
-    let headers = form_data.getHeaders();
-    headers['X-GAME-API-KEY'] = apikey || '';
-    let config = {
-        url,
-        method: 'post',
-        headers,
-        data: form_data
-    }
-    try {
-        let response = await axios.request(config);
-        console.log(response.data);
-    }
-    catch (e) {
-        console.error(e);
-    }
-
-}
-
-async function deployServerDatabase() {
-    let url = 'http://localhost:8080/api/v1/dev/update/server/db/';
-
-    console.log("Current Working Directory: ", process.cwd())
-    let filepath = path.resolve(process.cwd() + '/game-server/database.json');
-
-    try {
-        if (!fs.existsSync(filepath)) {
-            //file exists
-            console.warn('No database exists. It is optional, but this is a reminder just incase you forgot it.')
-            return;
-        }
-    } catch (err) {
-        console.error(err)
-    }
-
-    var newFile = fs.createReadStream(filepath);
-
-    var form_data = new FormData();
-    form_data.append('apikey', apikey || '');
-    form_data.append("db", newFile);
-
-    let headers = form_data.getHeaders();
-    headers['X-GAME-API-KEY'] = apikey || '';
-    let config = {
-        url,
-        method: 'post',
-        headers,
-        data: form_data
-    }
-    try {
-        let response = await axios.request(config);
-        console.log(response.data);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
-
-async function deployServer() {
-    let url = 'http://localhost:8080/api/v1/dev/update/server/bundle/';
-
-    console.log("Current Working Directory: ", process.cwd())
-    let filepath = path.resolve(process.cwd() + '/builds/server/server.bundle.js');
-    var newFile = fs.createReadStream(filepath);
-
-    var form_data = new FormData();
-    form_data.append('apikey', apikey || '');
-    form_data.append("bundle", newFile);
-
-    let headers = form_data.getHeaders();
-    headers['X-GAME-API-KEY'] = apikey || '';
-    let config = {
-        url,
-        method: 'post',
-        headers,
-        data: form_data
-    }
-    try {
-        let response = await axios.request(config);
-        console.log(response.data);
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
-
-async function deploy(type) {
-
-    switch (type) {
-        case 'client': deployClient(); break;
-        case 'server': deployServer(); break;
-        case 'db': deployServerDatabase(); break;
-        default:
-            deployClient();
-            deployServer();
-            deployServerDatabase();
-            break;
-    }
-
-
-
 }
 
 async function run() {

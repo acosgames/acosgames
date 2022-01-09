8a29fbb79b0bbf058ba9377c702cc5565a5b9879
Joel <joelruiz2@gmail.com>
2022-01-09 00:50:03 -0600

    fixed a few bugs

--- simulator/worker.js
+++ simulator/worker.js
@@ -116,7 +116,9 @@ class FSGWorker {
         if (typeof timer.set === 'undefined')
             return;
 
-        let seconds = Math.min(60, Math.max(10, timer.set));
+
+        let seconds = Math.min(3000000, Math.max(1, timer.set));
+        // let seconds = Math.min(60, Math.max(10, timer.set));
         let sequence = timer.seq || 0;
         let now = (new Date()).getTime();
         let deadline = now + (seconds * 1000);
